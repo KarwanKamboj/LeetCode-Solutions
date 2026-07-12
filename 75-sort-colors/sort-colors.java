445ad9d239ece639082c5bb5dc45fd85209a4a5f
KarwanKamboj <153383989+KarwanKamboj@users.noreply.github.com>
2026-07-12 08:27:13 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (60.65%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,15 +1,29 @@
 class Solution {
+    private void swap(int left,int right,int []nums){
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp ;
+        return ;
+    }
     public void sortColors(int[] nums) {
-        int n = nums.length ;
-        for(int i = 0 ; i< n ;i++){
-            for(int j = i+1; j<n;j++){
-                if(nums[i] > nums[j]){
-                    int temp = nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = temp;
-                }
+        // three pointer
+        int left = 0 ;
+        int mid = 0;
+        int right = nums.length -1 ;
+        while(mid<= right){
+            if(nums[mid] == 0){
+                swap(mid,left,nums);
+                left++;
+                mid++;
+            }
+            else if( nums[mid] == 1){
+                mid ++ ;
+            }
+            else{
+                // nums[mid] == 2
+                swap(mid,right,nums);
+                right--;
             }
         }
-        return ;
     }
 }
