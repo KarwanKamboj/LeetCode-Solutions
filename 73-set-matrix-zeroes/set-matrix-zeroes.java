4989313f1e06064251a2433abcfda293a45215aa
KarwanKamboj <153383989+KarwanKamboj@users.noreply.github.com>
2026-07-14 15:44:57 +0530

    Time: 1 ms (98.62%) | Memory: 47.6 MB (43.52%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;   // no. of rows
+        int n = matrix[0].length;  // no. of columns
+
+        boolean rows[] = new boolean[m];
+        boolean cols[] = new boolean[n];
+        for(int  i = 0 ; i<m ;i++){
+            for(int j = 0 ; j<n ;j++){
+                if(matrix[i][j] == 0){
+                    rows[i] = true;
+                    cols[j] = true ;
+                }
+            }
+        }
+        for(int i = 0 ;i < m ;i++){
+            for(int j =0 ; j < n ;j++){
+                if(rows[i] || cols[j] == true){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        return ;
+    }
+}
