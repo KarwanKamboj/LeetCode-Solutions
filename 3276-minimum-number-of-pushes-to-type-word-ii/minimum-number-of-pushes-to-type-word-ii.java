fa46f170b1989e3a9660d8ca34aa3669146a54f8
KarwanKamboj <153383989+KarwanKamboj@users.noreply.github.com>
2026-08-01 04:34:12 +0530

    Time: 9 ms (91.52%) | Memory: 48.1 MB (34.06%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
@@ -1,26 +1,24 @@
 class Solution {
     public int minimumPushes(String word) {
         int freq[] = new int[26];
-        for(char i :word.toCharArray()){
-            freq[i-'a']++;
+        for (char i : word.toCharArray()) {
+            freq[i - 'a']++;
         }
         Arrays.sort(freq);
-        int count =0;
-        for(int i = freq.length-1;i>=0;i--){
-            if(freq[i]==0)  break;
+        int count = 0;
+        
+        for (int i = freq.length - 1; i >= 0; i--) {
+            if (freq[i] == 0)
+                break;
 
-            if(i>17){
-                count+=freq[i]*1;
-            }
-            else if(i>9){
-                count+=freq[i]*2;
-            }
-            else if(i>1){
-                count+=freq[i]*3;
-            }
-            else{
-                count+=freq[i]*4;
-            }
+            if (i >= 18)
+                count += freq[i];
+            else if (i >= 10)
+                count += freq[i] * 2;
+            else if (i >= 2)
+                count += freq[i] * 3;
+            else
+                count += freq[i] * 4;
         }
         return count;
     }
