6e7d21de83b30749d514906c258e4f0a0f9079f5
KarwanKamboj <153383989+KarwanKamboj@users.noreply.github.com>
2026-08-01 23:44:42 +0530

    Time: 3 ms (80.80%) | Memory: 43.6 MB (67.74%) - LeetSync

--- 43-multiply-strings/multiply-strings.java
+++ 43-multiply-strings/multiply-strings.java
@@ -5,24 +5,24 @@ class Solution {
         if (num1.equals("0") || num2.equals("0"))
             return "0";
         int m = num1.length();
-        int n  = num2.length();
-        int res[] = new int[m+n];
-        
-        for(int i=m-1;i>=0;i--){
-            for(int j=n-1;j>=0;j--){
-                int mul=(num1.charAt(i)- '0')*(num2.charAt(j)- '0');
-                int digit = i+j;
-                int carry = i+j+1;
-               
-                int sum = mul+res[carry];
-                res[carry] = sum%10;
-                res[digit] = res[digit]+sum/10;
+        int n = num2.length();
+        int res[] = new int[m + n];
+
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+                int left = i + j;
+                int right = i + j + 1;
+                
+                int sum = mul+res[right];
+                res[right] = sum % 10; // store digit
+                res[left] = res[left]+ sum / 10; // store carry
             }
         }
         StringBuilder sb = new StringBuilder();
         for (int x : res) {
             if (sb.length() == 0 && x == 0) {
-                continue;   // skip leading zero
+                continue; // skip leading zero
             }
             sb.append(x);
         }
