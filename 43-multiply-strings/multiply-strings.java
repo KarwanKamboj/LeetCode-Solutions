2d6e0e32d706cf37a0993809d2fac223f37a38dd
KarwanKamboj <153383989+KarwanKamboj@users.noreply.github.com>
2026-08-02 10:34:49 +0530

    Time: 3 ms (80.65%) | Memory: 43.3 MB (96.28%) - LeetSync

--- 43-multiply-strings/multiply-strings.java
+++ 43-multiply-strings/multiply-strings.java
@@ -1,32 +1,33 @@
 class Solution {
-    // same approach as done on notebook
-    // t = O(m*n), s = O(m+n)
     public String multiply(String num1, String num2) {
-        if (num1.equals("0") || num2.equals("0"))
+        if (num1.equals("0") || num2.equals("0")) {
             return "0";
+        }
         int m = num1.length();
         int n = num2.length();
-        int res[] = new int[m + n];
+        int[] res = new int[m + n];
 
-        for (int i = m - 1; i >= 0; i--) {
-            for (int j = n - 1; j >= 0; j--) {
-                int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
-                int left = i + j;
-                int right = i + j + 1;
-                
+        // Outer loop on num2
+        for (int i = n - 1; i >= 0; i--) {
+            // Inner loop on num1
+            for (int j = m - 1; j >= 0; j--) {
+                int mul = (num2.charAt(i) - '0') * (num1.charAt(j) - '0');
+
+                int left=i+j;
+                int right=i+j+1;
+
                 int sum = mul+res[right];
-                res[right] = sum % 10; // store digit
-                res[left] = res[left]+ sum / 10; // store carry
+                res[right]=sum%10;
+                res[left]=res[left]+sum/10;
             }
         }
-        StringBuilder sb = new StringBuilder();
-        for (int x : res) {
-            if (sb.length() == 0 && x == 0) {
-                continue; // skip leading zero
+        StringBuilder sb=new StringBuilder();
+        for (int digit : res) {
+            if (sb.length()==0 && digit==0) {
+                continue;
             }
-            sb.append(x);
+            sb.append(digit);
         }
         return sb.toString();
     }
-
 }
