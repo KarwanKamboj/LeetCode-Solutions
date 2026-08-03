d809d60d892a4b04cd31dc03db235f7a31dc24df
KarwanKamboj <153383989+KarwanKamboj@users.noreply.github.com>
2026-08-03 16:22:18 +0530

    Time: 1 ms (75.49%) | Memory: 42.8 MB (51.43%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.java
+++ 225-implement-stack-using-queues/implement-stack-using-queues.java
@@ -1,21 +1,21 @@
-class MyStack {
+class MyStack { // space = O(1) 
     Queue<Integer> q;
     public MyStack() {
         q = new LinkedList<>();
     }
     public void push(int x) {
         q.add(x);
-        for (int i = 0; i < q.size() - 1; i++) {
+        for (int i = 0; i < q.size() - 1; i++) {  //t = O(n)
             q.add(q.poll());
         }
     }
     public int pop() {
-        return q.poll();
+        return q.poll();  // t = O(1)
     }
     public int top() {
-        return q.peek();
+        return q.peek();   // t = O(1)
     }
     public boolean empty() {
-        return q.isEmpty();
+        return q.isEmpty();  // t = O(1)
     }
 }
