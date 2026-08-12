b9572aab6d4ad489f2da076ebc547467b484eed3
KarwanKamboj <153383989+KarwanKamboj@users.noreply.github.com>
2026-08-13 04:35:25 +0530

    Time: 6 ms (4.19%) | Memory: 48.1 MB (34.55%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,32 +1,20 @@
-class Solution { // T = O(N) , S = O(N)
+class Solution {
     public int trap(int[] height) {
-
-        Stack<Integer> st = new Stack<>();
-        int water = 0;
-
-        for (int i = 0; i < height.length; i++) {
-
-            while (!st.isEmpty() && height[i] > height[st.peek()]) {
-
+        // t= O(N), S = O(N) USING STACK
+        int n = height.length;
+        int water  = 0;
+        Stack<Integer>st=new Stack<>();
+        for(int i = 0 ; i<n;i++){
+            while(!st.isEmpty() && height[i]>height[st.peek()]){
                 int bottom = st.pop();
-
-                if (st.isEmpty()) {
-                    break;
-                }
-
+                if(st.isEmpty()) break;
                 int left = st.peek();
-
-                int width = i - left - 1;
-
-                int h = Math.min(height[left], height[i])
-                        - height[bottom];
-
+                int width = i-left - 1; // only walls
+                int h = Math.min(height[left],height[i]) - height[bottom];// height of empty space
                 water += width * h;
             }
-
             st.push(i);
         }
-
         return water;
     }
 }
