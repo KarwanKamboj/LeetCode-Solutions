809279e586651c2019a85859872e890e7552b625
KarwanKamboj <153383989+KarwanKamboj@users.noreply.github.com>
2026-09-20 04:55:23 +0530

    Time: 46 ms (73.23%) | Memory: 116 MB (21.34%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/non-overlapping-intervals.java
@@ -1,14 +1,14 @@
-class Solution {//greddy algo, t = O(n), s = O(1)
+class Solution {//greedy algo, t = O(n),s = O(1)
     public int eraseOverlapIntervals(int[][] intervals) {
         Arrays.sort(intervals,(a,b)->Integer.compare(a[1],b[1]));
-        int count = 0;
-        int end = intervals[0][1];
-        for(int i=1;i<intervals.length;i++){
-           if(intervals[i][0]<end){
-            count++;
-           }else{
-            end = intervals[i][1]; 
-           }
+        int end = intervals[0][1]; // end of each interval
+        int count=0;
+        for(int i = 1; i<intervals.length;i++){
+            if(intervals[i][0]<end){ // starting of inteval is less then end of previous
+                count++;
+            }else{
+                end = intervals[i][1];
+            }
         }
         return count;
     }
