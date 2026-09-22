f3e648d611142f3c8edf3a49495a1adf5b582b9f
KarwanKamboj <153383989+KarwanKamboj@users.noreply.github.com>
2026-09-22 14:04:13 +0530

    Time: 37 ms (24.80%) | Memory: 75.1 MB (71.17%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
@@ -1,21 +1,22 @@
-class Solution {
+class Solution { // t = O(nlog n), s = O(n)
     public int[] arrayRankTransform(int[] arr) {
-        int n = arr.length;   //HashMap
-        int copy[] = Arrays.copyOf(arr,n);
-
-        Arrays.sort(copy);
-        HashMap<Integer,Integer> mp =new HashMap<>();
-        int rank = 1;
-        for(int m : copy){
-            if(!mp.containsKey(m)){
-                mp.put(m,rank++);
-            }
-           
+        int n =arr.length;
+        int ar1[] = new int[n];
+        for(int i = 0 ; i<n;i++){
+            ar1[i] = arr[i];
         }
-        int res[] = new int[n];
-        for(int i = 0 ; i < n ;i++){
-            res[i] = mp.get(arr[i]);
+        Arrays.sort(ar1);
+       HashMap<Integer,Integer>map = new HashMap<>();
+       int rank = 1 ;
+       for(int i = 0 ;i<n;i++){
+        if(!map.containsKey(ar1[i])){
+            map.put(ar1[i],rank++);
         }
-        return res;
+       }
+       int res[] = new int[n];
+       for(int i = 0 ; i< n ;i++){
+            res[i] = map.get(arr[i]);
+       }
+       return res;
     }
 }
